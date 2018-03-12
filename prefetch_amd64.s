6530a36894e014332d8355dcde8d58328c759db2
Damian Gryski <damian@gryski.com>
2018-03-12 10:30:21 -0700

    initial import

---
+++ prefetch_amd64.s
@@ -0,0 +1,21 @@
+// +build !gccgo,!appengine,!noasm
+
+TEXT ·T0(SB),4,$0-8
+        MOVQ  p+0(FP), AX
+        PREFETCHT0 (AX)
+        RET
+
+TEXT ·T1(SB),4,$0-8
+        MOVQ  p+0(FP), AX
+        PREFETCHT1 (AX)
+        RET
+
+TEXT ·T2(SB),4,$0-8
+        MOVQ  p+0(FP), AX
+        PREFETCHT2 (AX)
+        RET
+
+TEXT ·NTA(SB),4,$0-8
+        MOVQ  p+0(FP), AX
+        PREFETCHNTA (AX)
+        RET
